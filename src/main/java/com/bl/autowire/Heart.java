97568a3a2d379496e1c969e3db575f9b9a5786fe
sujitbidawe <bidawesujit@gmail.com>
2020-05-05 01:10:00 +0530

    added: code to add dependency with use of autowire

---
+++ src/main/java/com/bl/autowire/Heart.java
@@ -0,0 +1,9 @@
+package com.bl.autowire;
+
+public class Heart {
+
+    public void pump(){
+        System.out.println("Your heart is pumping");
+        System.out.println("Alive");
+    }
+}
---
+++ src/main/java/com/bl/autowire/Human.java
@@ -0,0 +1,19 @@
+package com.bl.autowire;
+
+public class Human {
+
+    private Heart heart;
+
+    public void setHeart(Heart heart) {
+        this.heart = heart;
+    }
+
+    public void startPumping(){
+        if (heart != null) {
+            heart.pump();
+        }
+        else {
+            System.out.println("You are dead! ");
+        }
+    }
+}
